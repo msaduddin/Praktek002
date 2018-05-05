7520495851cfc6516ae1e68e2f334102b1a5a919
Muhammad Sa'duddin <mnavaled@gmail.com>
2018-05-06 00:18:12 +0800

    Membuat Attribute dan Behavior

--- src/praktek02/Regtangle.java
+++ src/praktek02/Regtangle.java
@@ -1,4 +1,12 @@
 package praktek02;
 public class Regtangle {
+    double panjang;
+    double lebar;
     
+    void cetakInfo(){
+        System.out.println("===================");
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar :   "+lebar);
+        System.out.println("===================");
+    }
 }
